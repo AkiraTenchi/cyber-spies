ab6174e2992040c09dceba94e160d7579e641e28
isa0801 <isa080199@gmail.com>
2021-07-17 15:58:09 +0200

    quick fix for register

--- sheeesh/src/components/Home/HomePage.jsx
+++ sheeesh/src/components/Home/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useSelector} from "react-redux";
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import './HomePage.css';
@@ -8,11 +9,12 @@ import './HomePage.css';
 
 const HomePage = ()=> {
 
+    const state = useSelector((state)=>state)
 
     return(
         <div className="HomePageDiv MainBg">
 
-            <h1>hello</h1>
+            <h1>hello {state.account.username}</h1>
 
         </div>
         )
--- sheeesh/src/components/Login/Login.jsx
+++ sheeesh/src/components/Login/Login.jsx
@@ -36,7 +36,7 @@ const Login = ()=> {
 
         if(success){
             login({
-                "Username":Username
+                "username":Username
             })
             history.push("/")
         } else{
